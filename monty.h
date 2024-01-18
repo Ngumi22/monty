d117e8e0a64e67ce5baa463add31f7d6fdf75abc
Ngumi22 <mwaurapngumi@gmail.com>
2024-01-18 22:49:34 +0300

    Stacks, Queues - LIFO, FIFO

--- monty.h
+++ monty.h
@@ -53,4 +53,5 @@ typedef struct instruction_s
 } instruction_t;
 
 void monty_push(stack_t **stack, int value, unsigned int line_number);
+void monty_pall(stack_t **stack, unsigned int line_number);
 #endif
