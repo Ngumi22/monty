03860f3edad20b81d42462a39f23bfebb8e94606
Ngumi22 <mwaurapngumi@gmail.com>
2024-01-19 10:36:28 +0300

    Correction

--- monty.c
+++ monty.c
@@ -18,6 +18,7 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+		perror("Error");
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
