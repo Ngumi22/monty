ff89f61e6132ab759d4c567a508a6d39e0edc2cc
Ngumi22 <mwaurapngumi@gmail.com>
2024-01-19 10:11:01 +0300

    Correction

--- pall.c
+++ pall.c
@@ -8,11 +8,17 @@
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *current = *stack;
+	stack_t *current;
+	(void) line_number;
 
-	(void)line_number;
+	current = *stack;
 
-	while (current != NULL)
+	if (current == NULL)
+	{
+		return;
+	}
+
+	while (current)
 	{
 		printf("%d\n", current->n);
 		current = current->next;
