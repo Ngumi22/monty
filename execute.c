969fee2cca9a38a032c59aa27e38015f373f4d39
Ngumi22 <mwaurapngumi@gmail.com>
2024-01-19 10:15:44 +0300

    Correction

--- execute.c
+++ execute.c
@@ -22,32 +22,25 @@ void exec(stack_t **stack, char *opcode, unsigned int line_number, FILE *file)
 			exit(EXIT_FAILURE);
 		}
 		push(stack, value);
-	}
-	else if (strcmp(opcode, "pall") == 0)
+	} else if (strcmp(opcode, "pall") == 0)
 	{
 		pall(stack, line_number);
-	}
-	else if (strcmp(opcode, "pint") == 0)
+	} else if (strcmp(opcode, "pint") == 0)
 	{
 		pint(stack, line_number);
-	}
-	else if (strcmp(opcode, "pop") == 0)
+	} else if (strcmp(opcode, "pop") == 0)
 	{
 		pop(stack, line_number);
-	}
-	else if (strcmp(opcode, "swap") == 0)
+	} else if (strcmp(opcode, "swap") == 0)
 	{
 		swap(stack, line_number);
-	}
-	else if (strcmp(opcode, "add") == 0)
+	} else if (strcmp(opcode, "add") == 0)
 	{
 		add(stack, line_number);
-	}
-	else if (strcmp(opcode, "nop") == 0)
-        {
-                nop(stack, line_number);
-        }
-	else
+	} else if (strcmp(opcode, "nop") == 0)
+	{
+		nop(stack, line_number);
+	} else
 	{
 		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
 		cleanup(stack, file);
